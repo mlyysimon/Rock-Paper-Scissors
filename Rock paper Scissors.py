ab5e78a7defb7e52d78828a2828e64cacd509ab4
Michelle Simon <158987524+mlyysimon@users.noreply.github.com>
2024-02-04 21:56:13 -0800

    Update Rock paper Scissors.py

--- Rock paper Scissors.py
+++ Rock paper Scissors.py
@@ -3,7 +3,8 @@ import random
 def play():
     user = input("(R) for Rock, (P) for Paper, (S) for Scissors: ").lower()
     computer = random.choice(['r', 'p', 's'])
-    
+
+    # determining the different outcomes after computer and player choose a move
     if user == computer:
         return f"It's a tie! You both chose {written(computer)}"
     if wincondition(user,computer):
@@ -12,16 +13,19 @@ def play():
     return f"You lost! The computer chose {written(computer)}"
 
 def wincondition(player, opponent):
+    # rock > scissors, scissors > paper, paper > rock
     if (player == 'r' and opponent == 's') or {player == 's' and opponent == 'p'} or {player == 'p' and opponent == 'r'}:
         return True
 
 def playagain():
+    # choose to play again after a round of rock paper scissors
     playagain = input("Do you want to play again? (y/n): ")
     if playagain == "y":
         main()
     else:
         pass
-    
+        
+# returns the choice of player after winner is determined
 def written(choice):
     match choice:
         case "r":
